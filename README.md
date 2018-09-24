22fe8c311382db41da42a66772502746fa2aeca4
Charlie J Zhang <charlie.j.zhang@pwc.com>
2018-09-24 15:13:17 +0800

    add Swiper

---
+++ README.md
@@ -0,0 +1,6 @@
+# FMN Charlie's Friday Movie Night
+
+
+## Features
+
+1. 首页内容列表的封面尺寸比例应该为16：9.
--- pages/index/index.js
+++ pages/index/index.js
@@ -9,9 +9,49 @@ Page({
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    swiper_imgs: [
+      "/images/FMN_posters/FMN0831.jpg",
+      "/images/FMN_posters/FMN0831.jpg",
+      "/images/FMN_posters/FMN0824.jpg"
+    ],
+    indicatorDots: true,
+    vertical: false,
+    autoplay: true,
+    circular: false,
+    interval: 2000,
+    duration: 500,
+    previousMargin: 0,
+    nextMargin: 0,
     image_list: ["/images/FMN_posters/FMN0914.png",
      "/images/FMN_posters/FMN0831.jpg", 
-     "/images/FMN_posters/FMN0824.jpg"]
+     "/images/FMN_posters/FMN0824.jpg"],
+    test:"#000000"
+  },
+  changeProperty: function (e) {
+    var propertyName = e.currentTarget.dataset.propertyName
+    var newData = {}
+    newData[propertyName] = e.detail.value
+    this.setData(newData)
+  },
+  changeIndicatorDots: function (e) {
+    this.setData({
+      indicatorDots: !this.data.indicatorDots
+    })
+  },
+  changeAutoplay: function (e) {
+    this.setData({
+      autoplay: !this.data.autoplay
+    })
+  },
+  intervalChange: function (e) {
+    this.setData({
+      interval: e.detail.value
+    })
+  },
+  durationChange: function (e) {
+    this.setData({
+      duration: e.detail.value
+    })
   },
   //事件处理函数
   bindViewTap: function() {
